216746262f30ade04e3fd7c765d415e194fe654f
Christian Thiel <christian.thiel@holisticon.de>
2023-10-20 10:08:03 +0200

    chore: Add documentation to methods and classes #8

--- extension/core/src/main/kotlin/io/holixon/axon/projection/adhoc/EventApplier.kt
+++ extension/core/src/main/kotlin/io/holixon/axon/projection/adhoc/EventApplier.kt
@@ -2,11 +2,24 @@ package io.holixon.axon.projection.adhoc
 
 import org.axonframework.eventhandling.DomainEventMessage
 
+/**
+ * The eventApplier is responsible for applying domain events to the current model instance.
+ */
 class EventApplier<T>(
   private val modelInspector: ModelInspector<T>
 ) {
 
-  fun <E> applyEvent(model: T, event: DomainEventMessage<E>) : T {
+  /**
+   * Tries to apply the given event to the given model instance.
+   * <ul>
+   *   <li>if no annotated MessageHandler is found for the domainEvent's payload type, the model will be returned untouched. </li>
+   *   <li>if an annotated MessageHandler is found the event will be applied using that method. If the return value of the method call is
+   *   again a model instance, that instance will be returned (immutable pattern), otherwise the original model instance (mutable pattern)
+   *
+   * @param model the current model instance
+   * @param event the domain event to apply
+   */
+  fun <E> applyEvent(model: T, event: DomainEventMessage<E>): T {
     return modelInspector.findEventHandler(event.payloadType)
       ?.handle(event, model) as T // assured in inspector that the only return type allowed return type is model class
       ?: model
--- extension/core/src/main/kotlin/io/holixon/axon/projection/adhoc/ModelFactory.kt
+++ extension/core/src/main/kotlin/io/holixon/axon/projection/adhoc/ModelFactory.kt
@@ -3,24 +3,36 @@ package io.holixon.axon.projection.adhoc
 import org.axonframework.eventhandling.DomainEventMessage
 import org.axonframework.eventsourcing.eventstore.DomainEventStream
 
+/**
+ * The model factory is responsible for instantiating the model.
+ */
 class ModelFactory<T>(
   private val modelInspector: ModelInspector<T>
 ) {
 
+  /**
+   * Takes the event stream and tries to construct a model instance. Either by taking the first event from the stream and using a
+   * constructor capable of accepting this event or by using a default constructor. Then the first event is used for construction,
+   * that event is marked as consumed in the stream.
+   *
+   * @param events the event stream
+   * @return an instance of the model class
+   * @throws NoSuchMethodException when no usable constructor is found
+   */
   fun createInstanceFromStream(events: DomainEventStream): T {
     val firstEvent = events.peek()
 
-    if (canCreateFromEvent(firstEvent.payloadType)) {
+    return if (canCreateFromEvent(firstEvent.payloadType)) {
       val instance = createInstance(firstEvent)
       events.next() // mark first event as consumed
-      return instance
+      instance
     } else {
       // do not mark first event as consumed
-      return createInstance()
+      createInstance()
     }
   }
 
-  fun createInstance(message: DomainEventMessage<*>): T {
+  private fun createInstance(message: DomainEventMessage<*>): T {
     modelInspector.findConstructor(message.payloadType)?.let {
       return it.handle(message, null) as T
     }
@@ -28,10 +40,10 @@ class ModelFactory<T>(
     throw NoSuchMethodException("No suitable constructor found for payload ${message.payloadType}")
   }
 
-  fun createInstance(): T {
+  private fun createInstance(): T {
     return modelInspector.getDefaultConstructor().newInstance()
   }
 
-  internal fun canCreateFromEvent(eventClazz: Class<*>): Boolean =
+  private fun canCreateFromEvent(eventClazz: Class<*>): Boolean =
     modelInspector.findConstructor(eventClazz) != null
 }
